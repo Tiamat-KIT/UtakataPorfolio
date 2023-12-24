7daa2d056dc647a49e9d8e0f994bbf6538a24da6
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2023-12-24 17:50:22 +0000

    保存

--- portfilio/app/routes/_index.tsx
+++ portfilio/app/routes/_index.tsx
@@ -34,13 +34,16 @@ export default function Index() {
         <div className="col-span-3">
           <Carousel />
           <article>
-            <h3 className="hero text-2xl">道のり</h3>
-            <div className="grid grid-cols-5 grid-rows-1">
+            <h3 className="hero text-2xl">使える技術</h3>
+            <h5 className="hero text-lg">プログラミング言語</h5>
+            <hr />
+
+            {/* <div className="grid grid-cols-5 grid-rows-1">
               <div className="col-span-3">
                 
               </div>
               <div className="col-span-2">
-              {/* <ul className="timeline timeline-vertical">
+              <ul className="timeline timeline-vertical">
                 <li>
                   <div className="timeline-middle">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
@@ -71,9 +74,9 @@ export default function Index() {
                         </li>
                     )
                 })}
-              </ul> */}
+              </ul> 
               </div>
-            </div>
+            </div>*/}
           </article>
         </div>
       <div className="border-l-[1.0px] border-black h-full">
